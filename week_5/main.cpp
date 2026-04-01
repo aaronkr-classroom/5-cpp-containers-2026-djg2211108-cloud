79eebb71cea7e1eb974dd353ce76842a60dfb653
ththfql <145094239+ththfql@users.noreply.github.com>
2026-04-01 16:53:33 +0900

    sd

---
+++ week_5/main.cpp
@@ -0,0 +1,12 @@
+//main.cpp
+#include<iostream>
+#include<vector>
+#include<string>
+
+
+using namespace std;
+
+int main(void) {
+	cout << "Program runs!" << endl;
+		return 0;
+}
---
+++ week_5/split_str.h
@@ -0,0 +1,4 @@
+#pragma once
+//split_str.h
+
+#ifndef GU
